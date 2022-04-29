afe86b8a46d5b6697f8ecf4f42f4c764ddd81c64
Yurii Mahotskyi <ymah@softserveinc.com>
2022-04-29 10:00:38 +0300

    Core structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// ./containers/App.js
+import React from 'react';
+import { hot } from 'react-hot-loader';
+ 
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
     </div>
   );
 }
-
-export default App;
+ 
+export default hot(module)(App);
